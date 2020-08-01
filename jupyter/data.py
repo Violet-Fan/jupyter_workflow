43902aeddeccac41f59d5cd167066ef50ea0ab54
violet <violet@violetdeMacBook-Air.local>
2020-08-01 10:31:39 +0800

    set URL and FILENAME constants

--- jupyter/data.py
+++ jupyter/data.py
@@ -2,20 +2,10 @@ import os
 import pandas as pd 
 from urllib.request import urlretrieve
 
-def get_fremont_bicycle_data(filename, url, force_download=False):
-    ##mm force_download=False, os.path.exists
-    if force_download or not os.path.exists(filename):
-        urlretrieve(url, filename)
-    data = pd.read_csv('fremont_bicycle.csv', index_col='Date', parse_dates=True)
-    ## index_col, parse_dates(Timestamp)
-    data.columns = ['Total', 'East', 'West']
-    ## the columns changed
-    return data 
-import os 
-import pandas as pd 
-from urllib.request import urlretrieve
+URL = 'https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD'
+FILENAME = 'fremont_bicycle.csv'
 
-def get_fremont_bicycle_data(filename, url, force_download=False):
+def get_fremont_bicycle_data(filename = FILENAME , url = URL , force_download = False):
     '''
     download and cache the data
     parameters:
