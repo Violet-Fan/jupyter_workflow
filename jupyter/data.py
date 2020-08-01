f301861cb94765b3bc53aeb5b887d80d2afdcf9a
violet <violet@violetdeMacBook-Air.local>
2020-08-01 13:52:53 +0800

    refactor data download

--- jupyter/data.py
+++ jupyter/data.py
@@ -2,8 +2,8 @@ import os
 import pandas as pd 
 from urllib.request import urlretrieve
 
-URL = 'https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD'
 FILENAME = 'fremont_bicycle.csv'
+URL = 'https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD'
 
 def get_fremont_bicycle_data(filename = FILENAME , url = URL , force_download = False):
     '''
@@ -19,8 +19,12 @@ def get_fremont_bicycle_data(filename = FILENAME , url = URL , force_download = 
     ##mm force_download=False, os.path.exists
     if force_download or not os.path.exists(filename):
         urlretrieve(url, filename)
-    data = pd.read_csv('fremont_bicycle.csv', index_col='Date', parse_dates=True)
-    ## index_col, parse_dates(Timestamp)
+    data = pd.read_csv('fremont_bicycle.csv', index_col='Date')
+    try: 
+        data.index = pd.to_datetime(data.index, format='%m/%d/%Y %I:%M:%S %p')
+    except TypeError: 
+        data.index = pd.to_datetime(data.index)  
+    ## index_col, parse_dates = True(Timestamp): time consuming
     data.columns = ['Total', 'East', 'West']
     ## the columns changed
     return data 
